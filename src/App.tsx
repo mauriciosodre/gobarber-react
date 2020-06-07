6264d65849191c1a608ffe9b5f581630b5fd918f
Mauricio Sodre <m.sodre8@gmail.com>
2020-06-07 17:52:39 -0300

    signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn/index';
+// import SignIn from './pages/SignIn/index';
+import SignUp from './pages/SignUp/index';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
